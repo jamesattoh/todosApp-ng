65574510ef0ac8bf5372f249b467d2e5dbe5c54c
jamesattoh <koss.attoh27@gmail.com>
2025-11-28 04:42:13 +0100

    implement controls on the auth form & hiding/showing password

--- src/app/common/components/authentification/authentification.component.ts
+++ src/app/common/components/authentification/authentification.component.ts
@@ -13,11 +13,17 @@ export class AuthentificationComponent {
   // rendons dynamique le titre du formulaire
   readonly formTitle = input.required<'Connexion' | 'Inscription'>();
 
+  protected passwordType: 'password' | 'text' = 'password';
+
   // representation objet de mon formulaire
   protected authentificationForm = new FormGroup({
     email: new FormControl('', [ Validators.required, Validators.email ]),
     password: new FormControl('', [ Validators.required, Validators.minLength(8)]),
   })
+
+  public togglePasswordVisibility(): void {
+    if (this.passwordType === 'password') { this.passwordType = 'text'; } else { this.passwordType = 'password'; }
+  }
 
   public submitAuthentificationForm(): void {
     console.log('Submit Authentification');
